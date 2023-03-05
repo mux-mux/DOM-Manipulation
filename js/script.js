629f02f38aa48c7f232b724ca2c72cf8d0b17d7a
mux-mux <kmmyemail@ukr.net>
2023-03-05 15:52:34 +0200

    fea-add_films_into_list

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
@@ -28,7 +31,33 @@ window.onload = function () {
         content = document.querySelector('.promo__content'),
         bg = document.querySelector('.promo__bg'),
         genre = bg.querySelector('.promo__genre'),
-        films = document.querySelector('.promo__interactive-list');
+        films = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('.add'),
+        inputedFilm = document.querySelector('.adding__input'),
+        deleteIcon = document.querySelectorAll('.promo__interactive-item'),
+        isFavorite = document.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let inpFilm = inputedFilm.value;
+        let isFavorieFilm = isFavorite.checked;
+        if (inpFilm.length > 21) {
+            movieDB.movies.push(inpFilm.slice(0, 21) + '...');
+        } else {
+            movieDB.movies.push(inpFilm);
+        }
+        checkFavorite(isFavorieFilm);
+        showMovieList(movieDB.movies);
+    });
+
+    deleteIcon.forEach((item, i) => {
+        item.addEventListener('click', function (e) {
+            console.log(item.parentElement);
+            // e.target.parentElement.remove();
+            // e.target.removeEventListener();
+        });
+    });
+
 
     adv.remove();
     // content.style.cssText = 'width:calc(100%-300px);';
@@ -38,13 +67,19 @@ window.onload = function () {
 
     bg.style.background = 'url("img/bg.jpg") center center/cover no-repeat';
 
-    movieDB.movies.sort();
-    films.innerHTML = '';
-    movieDB.movies.forEach((item, i) => {
-        films.innerHTML += `
-        <li class="promo__interactive-item">
-            ${i + 1}. ${item}
-            <div class="delete"></div>
-        </li>`;
-    });
+    function checkFavorite(isChecked) {
+        (isChecked) ? console.log('Add fav film') : console.log('fav unchecked');;
+    }
+    function showMovieList(moviesList) {
+        moviesList.sort();
+        films.innerHTML = '';
+        moviesList.forEach((item, i) => {
+            films.innerHTML += `
+            <li class="promo__interactive-item">
+                ${i + 1}. ${item}
+                <div class="delete"></div>
+            </li>`;
+        });
+    }
+    showMovieList(movieDB.movies);
 };
